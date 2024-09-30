694ac3fed7fec41490f650c7e02e655bef99ae56
Toporets-d-a <toporets.dima.4002.10.90@gmail.com>
2024-09-30 22:49:03 +0300

    Daily save

---
+++ sci_work_front/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import AppHeader from './Components/AppHeader'
+
+function App() {
+  return (
+    <div className="App">
+      <AppHeader/>
+      <nav className="App-menu">
+
+      </nav>
+      <main className="App-content">
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ sci_work_front/src/Components/AppHeader.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../Styles/AppHeader.sass';
+
+const AppHeader = () => {
+    return (
+        <header>
+            <div class="logo">
+                <img src="../logo.svg" alt="SciWork"/>
+            </div>
+            <ul class="menu">
+                <li><a href="#lifestyle" class="menu_link"></a></li>
+            </ul>
+        </header>
+    )}
+
+export default AppHeader
